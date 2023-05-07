0282a002de78dc98c080c58b3f618f15725457d6
Dhruv Sharma <dhruvamsh@gmail.com>
2023-05-07 08:39:33 +0530

    Update Conversation

--- client/src/components/Conversation.jsx
+++ client/src/components/Conversation.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import {useSelector} from "react-redux"
-import axios from "axios"
-
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const Person = styled.div`
   display: flex;
@@ -21,81 +20,74 @@ const Person = styled.div`
     background-color: #3d3dae;
   }
 
- 
-
-    img {
-      height: 60px;
-      width: 60px;
-      border-radius: 50%;
-      cursor: pointer;
-    }
-  
+  img {
+    height: 60px;
+    width: 60px;
+    border-radius: 50%;
+    cursor: pointer;
+  }
 `;
 
-const Left=styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-gap: 10px;
+const Left = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
 `;
 
-const Right=styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   height: 30px;
   width: 30px;
   border-radius: 50%;
-  background-color: ${(props)=>props.onlineUsers.findIndex((u)=>u.userId===props.user._id) === -1?"red":"blue"}
-  
+  background-color: ${(props) =>
+    props.onlineUsers.findIndex((u) => u.userId === props.user._id) === -1
+      ? "red"
+      : "blue"};
 `;
 
-const Conversation = ({conv,onlineUsers}) => {
+const Conversation = ({ conv, onlineUsers }) => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [user, setUser] = useState({});
 
-    const {currentUser}=useSelector((state)=>state.user);
-    const [user,setUser]=useState({});
+  const fetchUser = async () => {
+    const friends = conv.members.find((m) => m !== currentUser._id);
 
-    const fetchUser=async()=>{
-        const friends= conv.members.find((m)=>m!==currentUser._id)
-
-        try{
-            const res=await axios.get(`https://velle-wtov.onrender.com/api/users/find/${friends},{headers:{Authorization:"Bearer "+currentUser.jwt}}`);
-            setUser(res.data);
-        }
-        catch(err){
-            console.log(err);
-        }
+    try {
+      const res = await axios.get(
+        `https://velle-wtov.onrender.com/api/users/find/${friends}`,{headers:{Authorization:"Bearer "+currentUser.jwt}}
+      );
+      setUser(res.data);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-
-    useEffect(()=>{
-        fetchUser();
-    },[conv])
-
+  useEffect(() => {
+    fetchUser();
+  }, [conv]);
 
   return (
     <Person>
       <Left>
-
-      <div>
-        <img src={user.img || "/images/Person.svg"} alt="" />
-      </div>
-      <div>
-        <span
-          style={{
-            color: "white",
-            fontSize: "20px",
-            fontWeight: "bold",
-          }}
+        <div>
+          <img src={user.img || "/images/Person.svg"} alt="" />
+        </div>
+        <div>
+          <span
+            style={{
+              color: "white",
+              fontSize: "20px",
+              fontWeight: "bold",
+            }}
           >
-          {user.name}
-        </span>
-        
-      </div>
-          </Left>
-          <Right onlineUsers={onlineUsers} user={user}>
-
-          </Right>
+            {user.name}
+          </span>
+        </div>
+      </Left>
+      <Right onlineUsers={onlineUsers} user={user}></Right>
     </Person>
   );
 };
